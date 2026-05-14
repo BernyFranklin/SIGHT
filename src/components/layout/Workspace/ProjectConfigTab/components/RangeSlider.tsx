b15c8fa8c98c63dae8a5cb96245e87cf9d581e2b
Frank Bernal <frankbernal@mail.fresnostate.edu>
2026-05-13 21:00:43 -0700

    Added track for sliders.

--- src/components/layout/Workspace/ProjectConfigTab/components/RangeSlider.tsx
+++ src/components/layout/Workspace/ProjectConfigTab/components/RangeSlider.tsx
@@ -23,7 +23,11 @@ export function RangeSlider({
         max={max}
         step={step}
         onChange={(e) => onChange(Number(e.target.value))}
-        className="h-1 flex-1 cursor-pointer appearance-none rounded-full bg-surface-raised accent-primary"
+        className="h-4 flex-1 cursor-pointer appearance-none bg-transparent
+          [&::-webkit-slider-runnable-track]:h-1 [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-runnable-track]:bg-surface
+          [&::-webkit-slider-thumb]:-mt-1.5 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-primary
+          [&::-moz-range-track]:h-1 [&::-moz-range-track]:rounded-full [&::-moz-range-track]:bg-surface-raised
+          [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:w-4 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:border-0 [&::-moz-range-thumb]:bg-primary"
       />
       <span className="w-16 shrink-0 text-right font-mono text-sm tabular-nums text-text">{display}</span>
     </div>
